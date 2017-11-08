09c6444f9da0dd03dd4158376e527f96da831e0e
firog <olofmarkstedt@gmail.com>
2017-11-08 16:42:14 +0100

    mongodb uses stateful sets

--- clean.sh
+++ clean.sh
@@ -4,3 +4,5 @@ kubectl delete rc --all
 kubectl delete deployment --all
 kubectl delete svc --all
 kubectl delete ing --all
+kubectl delete statefulsets --all
+# kubectl delete pvc --all
--- orchestrate.sh
+++ orchestrate.sh
@@ -3,11 +3,12 @@
 kubectl run nginx --image=nginx --port=80
 kubectl expose deployment nginx --target-port=80 --type=NodePort
 
-kubectl create -f mongodb-controller.yaml
+# kubectl create -f mongodb-controller.yaml
+kubectl create -f mongodb-statefulset.yaml
 kubectl create -f mongodb-service.yaml
 
-#kubectl create -f customer-deployment.yaml
-#kubectl create -f customer-service.yaml
+kubectl create -f customer-controller.yaml
+kubectl create -f customer-service.yaml
 
 kubectl create -f store-deployment.yaml
 kubectl create -f store-service.yaml
